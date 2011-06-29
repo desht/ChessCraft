2279ec629e01b8d19cee2a7f040e91c0c848271c
Des Herriott <des.herriott@gmail.com>
2011-06-29 13:59:55 +0100

    just removed a TODO comment

--- src/me/desht/chesscraft/exceptions/ChessException.java
+++ src/me/desht/chesscraft/exceptions/ChessException.java
@@ -3,7 +3,7 @@ package me.desht.chesscraft.exceptions;
 @SuppressWarnings("serial")
 public class ChessException extends Exception {
 	public ChessException() {
-		// TODO Auto-generated constructor stub
+		
 	}
 	public ChessException(String message) {
 		super(message);
