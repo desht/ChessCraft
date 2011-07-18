133e2171ba89caaa8e07fbabad029b579df18541
Des Herriott <des.herriott@gmail.com>
2011-07-18 10:06:14 +0100

    Removed debugging print statement when teleporting to a game.

--- src/me/desht/chesscraft/ChessCommandExecutor.java
+++ src/me/desht/chesscraft/ChessCommandExecutor.java
@@ -458,7 +458,6 @@ public class ChessCommandExecutor implements CommandExecutor {
 			loc.setYaw(0.0f);
 			loc.add(-4.5 * bv.getSquareSize(), 2.0, 0.0);
 		}
-		System.out.println("go to " + loc);
 		if (loc.getBlock().getTypeId() != 0 || loc.getBlock().getFace(BlockFace.UP).getTypeId() != 0) {
 			throw new ChessException("Teleport destination obstructed!");
 		}
