ef319b1c2a089e7b1946eb617a48a704a910ffc6
Des Herriott <des.herriott@gmail.com>
2011-07-21 10:41:54 +0100

    Relax the piece size vs board square size restriction - pieces can now be as wide as the square (though not any wider).
    Ensure that shouldPlaceLast() block types are really placed last when
    drawing pieces to avoid blocks (torches etc.) popping off.

--- src/me/desht/chesscraft/BoardView.java
+++ src/me/desht/chesscraft/BoardView.java
@@ -63,11 +63,14 @@ public class BoardView implements PositionListener {
 		origin = where;
 		a1Square = calcBaseSquare(where);
 		validateIntersections();
+		System.out.println("intersections validated");
 		stones = createStones(pieceStyle);
+		System.out.println("stones created");
 		validateBoardParams();
 		controlPanel = new ControlPanel(plugin, this);
 		lastLevel = -1;
 
+		System.out.println("board created");
 		BoardView.addBoardView(name, this);
 	}
 
@@ -86,7 +89,7 @@ public class BoardView implements PositionListener {
 			maxH = Math.max(maxH, entry.getValue().sizeZ);
 			maxV = Math.max(maxV, entry.getValue().sizeY);
 		}
-		if (maxH >= squareSize)
+		if (maxH > squareSize)
 			throw new ChessException("Set '" + pieceStyle + "' is too wide for this board!");
 		if (maxV > height)
 			throw new ChessException("Set '" + pieceStyle + "' is too tall for this board!");
@@ -216,6 +219,7 @@ public class BoardView implements PositionListener {
 			plugin.library.loadChessSet(pieceStyle);
 		}
 		Map<Integer, ChessStone> result = new HashMap<Integer, ChessStone>();
+
 		for (int stone = Chess.MIN_STONE; stone <= Chess.MAX_STONE; stone++) {
 			if (stone != Chess.NO_STONE)
 				result.put(stone, plugin.library.getStone(pieceStyle, stone));
@@ -247,8 +251,7 @@ public class BoardView implements PositionListener {
 			y2 = 127;
 		World w = a1Square.getWorld();
 
-		Cuboid walls[] = {
-				new Cuboid(new Location(w, x1, y1, z2), new Location(w, x2, y2, z2)), // west
+		Cuboid walls[] = { new Cuboid(new Location(w, x1, y1, z2), new Location(w, x2, y2, z2)), // west
 				new Cuboid(new Location(w, x1, y1, z1), new Location(w, x2, y2, z1)), // east
 				new Cuboid(new Location(w, x1, y1, z1), new Location(w, x1, y2, z2)), // north
 				new Cuboid(new Location(w, x2, y1, z1), new Location(w, x2, y2, z2)), // south
@@ -272,8 +275,7 @@ public class BoardView implements PositionListener {
 		int x2 = bounds.getUpperSW().getBlockX();
 		int z2 = bounds.getUpperSW().getBlockZ();
 
-		Cuboid[] frameParts = {
-				new Cuboid(new Location(w, x1 - fw, y, z1 - fw), new Location(w, x2 + fw, y, z1)), // east
+		Cuboid[] frameParts = { new Cuboid(new Location(w, x1 - fw, y, z1 - fw), new Location(w, x2 + fw, y, z1)), // east
 				new Cuboid(new Location(w, x1 - fw, y, z2), new Location(w, x2 + fw, y, z2 + fw)), // west
 				new Cuboid(new Location(w, x1 - fw, y, z1 - fw), new Location(w, x1, y, z2 + fw)), // north
 				new Cuboid(new Location(w, x2, y, z1 - fw), new Location(w, x2 + fw, y, z2 + fw)), // south
@@ -309,18 +311,18 @@ public class BoardView implements PositionListener {
 		World w = a1Square.getWorld();
 		if (stone == Chess.NO_STONE) {
 
-                        // first remove blocks that might pop off & leave a drop
-                        Block b = null;
+			// first remove blocks that might pop off & leave a drop
+			Block b = null;
 			for (int x = 0; x < squareSize; x++) {
 				for (int y = 1; y <= height; y++) {
 					for (int z = 0; z < squareSize; z++) {
-                                            b = w.getBlockAt(l.getBlockX() - x, l.getBlockY() + y, l.getBlockZ() - z);
-                                            if(shouldPlaceLast(b.getTypeId())){
-                                                b.setTypeId(0);
-                                            }
-                                        }
-                                }
-                        }
+						b = w.getBlockAt(l.getBlockX() - x, l.getBlockY() + y, l.getBlockZ() - z);
+						if (shouldPlaceLast(b.getTypeId())) {
+							b.setTypeId(0);
+						}
+					}
+				}
+			}
 			for (int x = 0; x < squareSize; x++) {
 				for (int y = 1; y <= height; y++) {
 					for (int z = 0; z < squareSize; z++) {
@@ -329,7 +331,7 @@ public class BoardView implements PositionListener {
 				}
 			}
 		} else {
-			MaterialWithData air = new MaterialWithData(0, (byte) -1);
+			MaterialWithData air = new MaterialWithData(0, (byte) 0);
 			ChessStone cStone = stones.get(stone);
 			int xOff = (squareSize - cStone.getSizeX()) / 2;
 			int zOff = (squareSize - cStone.getSizeZ()) / 2;
@@ -337,9 +339,19 @@ public class BoardView implements PositionListener {
 				for (int y = 0; y < height; y++) {
 					for (int z = 0; z < cStone.getSizeZ(); z++) {
 						MaterialWithData mat = y >= cStone.getSizeY() ? air : cStone.getMaterial(x, y, z);
-						ChessCraft.setBlock(w.getBlockAt((l.getBlockX() - xOff) - x, l.getBlockY() + y + 1, (l
-								.getBlockZ() - zOff)
-								- z), mat);
+						if (!shouldPlaceLast(mat.material))
+							ChessCraft.setBlock(w.getBlockAt((l.getBlockX() - xOff) - x, l.getBlockY() + y + 1,
+							                                 (l.getBlockZ() - zOff)- z), mat);
+					}
+				}
+			}
+			for (int x = 0; x < cStone.getSizeX(); x++) {
+				for (int y = 0; y < height; y++) {
+					for (int z = 0; z < cStone.getSizeZ(); z++) {
+						MaterialWithData mat = y >= cStone.getSizeY() ? air : cStone.getMaterial(x, y, z);
+						if (shouldPlaceLast(mat.material))
+							ChessCraft.setBlock(w.getBlockAt((l.getBlockX() - xOff) - x, l.getBlockY() + y + 1,
+							                                 (l.getBlockZ() - zOff) - z), mat);
 					}
 				}
 			}
@@ -552,131 +564,131 @@ public class BoardView implements PositionListener {
 		delete(false, null);
 	}
 
-        /**
-         * Checks whether a block can be passed through.
-         * (copied from WorldEdit ... after i'd made another, lol)
-         * @param id
-         * @return
-         */
-        public static boolean canPassThrough(int id) {
-            return id == 0 // Air
-                    || id == 8 // Water
-                    || id == 9 // Water
-                    || id == 6 // Saplings
-                    || id == 27 // Powered rails
-                    || id == 28 // Detector rails
-                    || id == 30 // Web <- someone will hate me for this
-                    || id == 31 // Long grass
-                    || id == 32 // Shrub
-                    || id == 37 // Yellow flower
-                    || id == 38 // Red flower
-                    || id == 39 // Brown mushroom
-                    || id == 40 // Red mush room
-                    || id == 50 // Torch
-                    || id == 51 // Fire
-                    || id == 55 // Redstone wire
-                    || id == 59 // Crops
-                    || id == 63 // Sign post
-                    || id == 65 // Ladder
-                    || id == 66 // Minecart tracks
-                    || id == 68 // Wall sign
-                    || id == 69 // Lever
-                    || id == 70 // Stone pressure plate
-                    || id == 72 // Wooden pressure plate
-                    || id == 75 // Redstone torch (off)
-                    || id == 76 // Redstone torch (on)
-                    || id == 77 // Stone button
-                    || id == 78 // Snow
-                    || id == 83 // Reed
-                    || id == 90 // Portal
-                    || id == 93 // Diode (off)
-                    || id == 94; // Diode (on)
-        }
+	/**
+	 * Checks whether a block can be passed through. (copied from WorldEdit ...
+	 * after i'd made another, lol)
+	 * 
+	 * @param id
+	 * @return
+	 */
+	public static boolean canPassThrough(int id) {
+		return id == 0 // Air
+				|| id == 8 // Water
+				|| id == 9 // Water
+				|| id == 6 // Saplings
+				|| id == 27 // Powered rails
+				|| id == 28 // Detector rails
+				|| id == 30 // Web <- someone will hate me for this
+				|| id == 31 // Long grass
+				|| id == 32 // Shrub
+				|| id == 37 // Yellow flower
+				|| id == 38 // Red flower
+				|| id == 39 // Brown mushroom
+				|| id == 40 // Red mush room
+				|| id == 50 // Torch
+				|| id == 51 // Fire
+				|| id == 55 // Redstone wire
+				|| id == 59 // Crops
+				|| id == 63 // Sign post
+				|| id == 65 // Ladder
+				|| id == 66 // Minecart tracks
+				|| id == 68 // Wall sign
+				|| id == 69 // Lever
+				|| id == 70 // Stone pressure plate
+				|| id == 72 // Wooden pressure plate
+				|| id == 75 // Redstone torch (off)
+				|| id == 76 // Redstone torch (on)
+				|| id == 77 // Stone button
+				|| id == 78 // Snow
+				|| id == 83 // Reed
+				|| id == 90 // Portal
+				|| id == 93 // Diode (off)
+				|| id == 94; // Diode (on)
+	}
 
-        
-        /**
-         * Returns true if the block is a container block.
-         * (copied from WorldEdit)
-         * @param id
-         * @return
-         */
-        public static boolean isContainerBlock(int id) {
-            return id == 23 // Dispenser
-                    || id == 61 // Furnace
-                    || id == 62 // Furnace
-                    || id == 54; // Chest
-        }
-        
-        /**
-         * Checks to see whether a block should be placed last.
-         * (also copied from WorldEdit)
-         * (paintings are not blocks, and are not included...)
-         * @param id
-         * @return
-         */
-        public static boolean shouldPlaceLast(int id) {
-            return id == 6 // Saplings
-                    || id == 26 // Beds
-                    || id == 27 // Powered rails
-                    || id == 28 // Detector rails
-                    || id == 31 // Long grass
-                    || id == 32 // Shrub
-                    || id == 37 // Yellow flower
-                    || id == 38 // Red flower
-                    || id == 39 // Brown mushroom
-                    || id == 40 // Red mush room
-                    || id == 50 // Torch
-                    || id == 51 // Fire
-                    || id == 55 // Redstone wire
-                    || id == 59 // Crops
-                    || id == 63 // Sign post
-                    || id == 64 // Wooden door
-                    || id == 65 // Ladder
-                    || id == 66 // Minecart tracks
-                    || id == 68 // Wall sign
-                    || id == 69 // Lever
-                    || id == 70 // Stone pressure plate
-                    || id == 71 // Iron door
-                    || id == 72 // Wooden pressure plate
-                    || id == 75 // Redstone torch (off)
-                    || id == 76 // Redstone torch (on)
-                    || id == 77 // Stone button
-                    || id == 78 // Snow
-                    || id == 81 // Cactus
-                    || id == 83 // Reed
-                    || id == 90 // Portal
-                    || id == 92 // Cake
-                    || id == 93 // Repeater (off)
-                    || id == 94 // Repeater (on)
-                    || id == 96; // Trap door
-        }
+	/**
+	 * Returns true if the block is a container block. (copied from WorldEdit)
+	 * 
+	 * @param id
+	 * @return
+	 */
+	public static boolean isContainerBlock(int id) {
+		return id == 23 // Dispenser
+				|| id == 61 // Furnace
+				|| id == 62 // Furnace
+				|| id == 54; // Chest
+	}
 
-        /**
-         * delete blocks in bounds, but don't allow items to drop
-         * (paintings are not blocks, and are not included...)
-         * also does not scan the faces of the region for drops when the region is cleared
-         */
+	/**
+	 * Checks to see whether a block should be placed last. (also copied from
+	 * WorldEdit) (paintings are not blocks, and are not included...)
+	 * 
+	 * @param id
+	 * @return
+	 */
+	public static boolean shouldPlaceLast(int id) {
+		return id == 6 // Saplings
+				|| id == 26 // Beds
+				|| id == 27 // Powered rails
+				|| id == 28 // Detector rails
+				|| id == 31 // Long grass
+				|| id == 32 // Shrub
+				|| id == 37 // Yellow flower
+				|| id == 38 // Red flower
+				|| id == 39 // Brown mushroom
+				|| id == 40 // Red mush room
+				|| id == 50 // Torch
+				|| id == 51 // Fire
+				|| id == 55 // Redstone wire
+				|| id == 59 // Crops
+				|| id == 63 // Sign post
+				|| id == 64 // Wooden door
+				|| id == 65 // Ladder
+				|| id == 66 // Minecart tracks
+				|| id == 68 // Wall sign
+				|| id == 69 // Lever
+				|| id == 70 // Stone pressure plate
+				|| id == 71 // Iron door
+				|| id == 72 // Wooden pressure plate
+				|| id == 75 // Redstone torch (off)
+				|| id == 76 // Redstone torch (on)
+				|| id == 77 // Stone button
+				|| id == 78 // Snow
+				|| id == 81 // Cactus
+				|| id == 83 // Reed
+				|| id == 90 // Portal
+				|| id == 92 // Cake
+				|| id == 93 // Repeater (off)
+				|| id == 94 // Repeater (on)
+				|| id == 96; // Trap door
+	}
+
+	/**
+	 * delete blocks in bounds, but don't allow items to drop (paintings are not
+	 * blocks, and are not included...) also does not scan the faces of the
+	 * region for drops when the region is cleared
+	 */
 	void wipe() {
-            Block b = null;
-            // first remove blocks that might pop off & leave a drop
-            for (Location l : getOuterBounds()) {
-                b = l.getBlock();
-                if(shouldPlaceLast(b.getTypeId())){
-                    b.setTypeId(0);
-                }// also check if this is a container
-                else if(isContainerBlock(b.getTypeId())){
-                    BlockState state = b.getState();
-                    if (state instanceof org.bukkit.block.ContainerBlock) {
-                        org.bukkit.block.ContainerBlock chest = (org.bukkit.block.ContainerBlock)state;
-                        Inventory inven = chest.getInventory();
-                        inven.clear();
-                    }
-                }
-            }
-            // now wipe all (remaining) blocks
-            for (Location l : getOuterBounds()) {
-                    l.getBlock().setTypeId(0);
-            }
+		Block b = null;
+		// first remove blocks that might pop off & leave a drop
+		for (Location l : getOuterBounds()) {
+			b = l.getBlock();
+			if (shouldPlaceLast(b.getTypeId())) {
+				b.setTypeId(0);
+			}// also check if this is a container
+			else if (isContainerBlock(b.getTypeId())) {
+				BlockState state = b.getState();
+				if (state instanceof org.bukkit.block.ContainerBlock) {
+					org.bukkit.block.ContainerBlock chest = (org.bukkit.block.ContainerBlock) state;
+					Inventory inven = chest.getInventory();
+					inven.clear();
+				}
+			}
+		}
+		// now wipe all (remaining) blocks
+		for (Location l : getOuterBounds()) {
+			l.getBlock().setTypeId(0);
+		}
 	}
 
 	void restoreTerrain(Player player) {
