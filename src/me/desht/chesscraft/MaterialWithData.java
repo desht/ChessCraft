ce8816803eed87db1411ad01b9b9abed069d959c
Des Herriott <des.herriott@gmail.com>
2011-07-22 15:35:00 +0100

    Removed parseIdAndData() method.

--- src/me/desht/chesscraft/MaterialWithData.java
+++ src/me/desht/chesscraft/MaterialWithData.java
@@ -44,35 +44,6 @@ public class MaterialWithData {
 		}
 	}
 
-	static MaterialWithData parseIdAndData(String string) {
-		String[] items = string.split(":");
-		int mat;
-		byte data;
-
-		if (items[0].matches("^[0-9]+$")) {
-			mat = Integer.parseInt(items[0]);
-		} else {
-			Material m = Material.valueOf(items[0].toUpperCase());
-			if (m == null)
-				throw new IllegalArgumentException("unknown material " + items[0]);
-			mat = m.getId();
-		}
-		if (items.length < 2)
-			return new MaterialWithData(mat, (byte) -1);
-
-		if (items[1].matches("^[0-9]+$")) {
-			data = Byte.parseByte(items[1]);
-		} else if (mat == 35) { // wool
-			DyeColor d = DyeColor.valueOf(items[1].toUpperCase());
-			if (d == null)
-				throw new IllegalArgumentException("unknown dye colour " + items[0]);
-			data = d.getData();
-		} else {
-			throw new IllegalArgumentException("invalid data specification " + items[1]);
-		}
-		return new MaterialWithData(mat, data);
-	}
-
 	@Override
 	public String toString() {
 		String s = Material.getMaterial(material).toString();
