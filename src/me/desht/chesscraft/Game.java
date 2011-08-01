14e845ada5dd75f91d3dc486b675906a80df7690
Des Herriott <des.herriott@gmail.com>
2011-08-01 09:46:29 +0100

    Fixed nasty bug in Game class which completely broke persistence - games were not correctly re-initialised on reload.

--- src/me/desht/chesscraft/Game.java
+++ src/me/desht/chesscraft/Game.java
@@ -74,15 +74,16 @@ public class Game {
 		} else {
 			stake = 0.0;
 		}
+
+		cpGame = new chesspresso.game.Game();
 		setupChesspressoGame();
 
 		view.setGame(this);
+		
 		getPosition().addPositionListener(view);
 	}
 
 	private void setupChesspressoGame() {
-		cpGame = new chesspresso.game.Game();
-
 		// seven tag roster
 		cpGame.setTag(PGN.TAG_EVENT, getName());
 		cpGame.setTag(PGN.TAG_SITE, getView().getName() + " in Minecraftia");
@@ -157,7 +158,6 @@ public class Game {
 			stake = (Double) map.get("stake");
 		}
 
-		//try {
 		if (isAIPlayer(playerWhite)) {
 			aiPlayer = ChessAI.getNewAI(this, playerWhite, true);
 			playerWhite = aiPlayer.getName();
@@ -168,36 +168,21 @@ public class Game {
 			aiPlayer.init(false);
 		}
 
-		// Replay the move history to restore the saved board position.
-		// We do this instead of just saving the position so that the
-		// Chesspresso Game model
+		setupChesspressoGame();
+		
+		// Replay the move history to restore the saved board position.  We do this
+		// instead of just saving the position so that the Chesspresso Game model 
 		// includes a history of the moves, suitable for creating a PGN file.
 		for (short move : history) {
 			getPosition().doMove(move);
 		}
-		// repeat for the ai engine (doesn't support loading from fen)
+		// repeat for the ai engine (doesn't support loading from FEN)
 		if (aiPlayer != null) {
 			for (short move : history) {
 				aiPlayer.loadmove(Move.getFromSqi(move), Move.getToSqi(move));
 			}
 			aiPlayer.loadDone(); // tell ai to start on next move
 		}
-//		} catch (IllegalMoveException e) {
-//			// should only get here if the save file was corrupted - the history
-//			// is a list of moves which was already validated before the game was saved
-//			ChessCraftLogger.log(Level.WARNING, "can't restore move history for game " + getName()
-//					+ " - move history corrupted?" + "  (game will be deleted)");
-//			deletePermanently();
-//			return false;
-//		} catch (Exception e) {
-//			ChessCraftLogger.log(Level.WARNING, "Unexpected exception restoring game "
-//					+ getName() + "\n" + e.getMessage() + "  (game will be deleted)");
-//			// delete game
-//			deletePermanently();
-//			return false;
-//		}
-
-		setupChesspressoGame();
 
 		getPosition().addPositionListener(view);
 
