b4ffdd598aa9a062f6e8e0e98d1ac8a1fc169c01
Des Herriott <des.herriott@gmail.com>
2012-06-29 09:09:48 +0100

    Just a comment update

--- src/main/java/me/desht/chesscraft/controlpanel/AbstractSignButton.java
+++ src/main/java/me/desht/chesscraft/controlpanel/AbstractSignButton.java
@@ -14,6 +14,12 @@ public abstract class AbstractSignButton extends AbstractSignLabel {
 		this.permissionNode = permissionNode;
 	}
 
+	/**
+	 * Called when the sign is clicked by the player.  Any ChessException thrown by this method (and the abstract execute()
+	 * method that it calls) will ultimately be caught and reported to the player by the PlayerInteractEvent event handler.
+	 * 
+	 * @param event	The player interaction event as caught by the plugin's event handler
+	 */
 	public void onClicked(PlayerInteractEvent event) {
 		if (!isEnabled() || !isReactive()) return;
 		
