b27551dea62a16f655dec4e66d9a5b208b7a5ae7
Des Herriott <des.herriott@gmail.com>
2012-10-29 10:44:14 +0000

    "/chess redraw" now redraws the board the caller is standing on now "/chess redraw -all" redraws all boards

--- src/main/java/me/desht/chesscraft/commands/RedrawCommand.java
+++ src/main/java/me/desht/chesscraft/commands/RedrawCommand.java
@@ -7,6 +7,7 @@ import me.desht.dhutils.MiscUtil;
 import me.desht.dhutils.commands.AbstractCommand;
 
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
 
 public class RedrawCommand extends AbstractCommand {
@@ -15,23 +16,39 @@ public class RedrawCommand extends AbstractCommand {
 		super("chess red", 0, 1);
 		setPermissionNode("chesscraft.commands.redraw");
 		setUsage("/chess redraw [<board-name>]");
+		setOptions("all");
 	}
 
 	@Override
-	public boolean execute(Plugin plugin, CommandSender player, String[] args) throws ChessException {
+	public boolean execute(Plugin plugin, CommandSender sender, String[] args) throws ChessException {
 		if (args.length >= 1) {
+			// redraw named board
 			BoardView bv = BoardView.getBoardView(args[0]);
-			bv.reloadStyle();
-			bv.paintAll();
-			MiscUtil.statusMessage(player, Messages.getString("ChessCommandExecutor.boardRedrawn", bv.getName())); //$NON-NLS-1$
-		} else {
+			repaintBoard(bv);
+			MiscUtil.statusMessage(sender, Messages.getString("ChessCommandExecutor.boardRedrawn", bv.getName())); //$NON-NLS-1$
+		} else if (getBooleanOption("all")) {
+			// redraw ALL boards
 			for (BoardView bv : BoardView.listBoardViews()) {
-				bv.reloadStyle();
-				bv.paintAll();
+				repaintBoard(bv);
 			}
-			MiscUtil.statusMessage(player, Messages.getString("ChessCommandExecutor.allBoardsRedrawn")); //$NON-NLS-1$
+			MiscUtil.statusMessage(sender, Messages.getString("ChessCommandExecutor.allBoardsRedrawn")); //$NON-NLS-1$
+		} else {
+			// redraw board caller is standing on, if any
+			notFromConsole(sender);
+			Player player = (Player) sender;
+			BoardView bv = BoardView.partOfChessBoard(player.getLocation());
+			if (bv == null) {
+				throw new ChessException(Messages.getString("Designer.notOnBoard"));
+			}
+			repaintBoard(bv);
+			MiscUtil.statusMessage(sender, Messages.getString("ChessCommandExecutor.boardRedrawn", bv.getName())); //$NON-NLS-1$
 		}
 		return true;
+	}
+
+	private void repaintBoard(BoardView bv) {
+		bv.reloadStyle();
+		bv.paintAll();
 	}
 
 }
