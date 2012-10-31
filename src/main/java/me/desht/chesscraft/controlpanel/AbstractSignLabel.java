ab8ba99e10fa8c854a04d69d85bc9d9531cc3f07
Des Herriott <des.herriott@gmail.com>
2012-10-31 10:40:07 +0000

    Remove redundant BlockState check for control panel signs

--- src/main/java/me/desht/chesscraft/controlpanel/AbstractSignLabel.java
+++ src/main/java/me/desht/chesscraft/controlpanel/AbstractSignLabel.java
@@ -21,11 +21,11 @@ public abstract class AbstractSignLabel {
 	private final static String DISABLED_COLOUR = ChatColor.DARK_GRAY.toString();
 	private final static String INDICATOR_COLOUR = ChatColor.DARK_RED.toString();
 	private static final String NONREACTIVE_COLOUR = "";
-	
+
 	private final ControlPanel panel;
 	private final PersistableLocation loc;
 	private final String labelKey;
-	
+
 	public AbstractSignLabel(ControlPanel panel, String labelKey, int x, int y) {
 		this.labelKey = labelKey;
 		this.panel = panel;
@@ -45,34 +45,34 @@ public abstract class AbstractSignLabel {
 	public BoardView getView() {
 		return getPanel().getView();
 	}
-	
+
 	public ChessGame getGame() {
 		return getView().getGame();
 	}
-	
+
 	public boolean isReactive() {
 		return false;
 	}
-	
+
 	public boolean gameInState(GameState state) {
 		return getGame() != null && getGame().getState() == state;
 	}
-	
+
 	public String getLabelColour() {
 		if (!isEnabled()) return DISABLED_COLOUR;
 		if (!isReactive()) return NONREACTIVE_COLOUR;
 		return ENABLED_COLOUR;
 	}
-	
+
 	public String getIndicatorColour() {
 		if (!isEnabled()) return DISABLED_COLOUR;
 		return INDICATOR_COLOUR;
 	}
-	
+
 	public String[] getLabel() {
 		String[] text = getCustomSignText();
 		if (text != null) return text;
-		
+
 		if (labelKey == null) {
 			LogUtils.warning("Unknown label key: " + labelKey);
 			return new String[] { "???" };
@@ -89,10 +89,10 @@ public abstract class AbstractSignLabel {
 		for (String line : label.split(";", 4)) {
 			res[i++] = line;
 		}
-		
+
 		return res;
 	}
-	
+
 	/**
 	 * Override this in subclasses if the button/label needs some special
 	 * processing to display its text.
@@ -102,36 +102,32 @@ public abstract class AbstractSignLabel {
 	protected String[] getCustomSignText() {
 		return null;
 	}
-	
+
 	public void repaint() {
 		Block block = loc.getBlock();
-		
+
 		byte data = getSignDirection();
 		if (block.getType() != Material.WALL_SIGN || block.getData() != data) {
 			MaterialWithData.get("wall_sign:" + data).applyToBlock(block);
 		}
-		
-		if (block.getState() instanceof Sign) {
-			Sign sign = (Sign) block.getState();
-			String[] label = getLabel();
 
-			String col = getLabelColour();
-			
-			for (int i = 0; i < 4 && i < label.length; ++i) {
-				if (label[i].equals("="))
-					continue;				// '=' means leave the line as it is
-				if (label[i].startsWith("\u00a7")) {
-					sign.setLine(i, label[i]);
-				} else {
-					sign.setLine(i, col + label[i]);
-				}
+		Sign sign = (Sign) block.getState();
+		String[] label = getLabel();
+
+		String col = getLabelColour();
+
+		for (int i = 0; i < 4 && i < label.length; ++i) {
+			if (label[i].equals("="))
+				continue;				// '=' means leave the line as it is
+			if (label[i].startsWith("\u00a7")) {
+				sign.setLine(i, label[i]);
+			} else {
+				sign.setLine(i, col + label[i]);
 			}
-			sign.update();
-		} else {
-			LogUtils.warning("sign button isn't a sign! block: " + block);
 		}
+		sign.update();
 	}
-	
+
 	private PersistableLocation getSignLocation(int x, int y) {
 		BoardRotation boardRot = panel.getView().getRotation();
 		BoardRotation signRot = boardRot.getRight();
@@ -141,7 +137,7 @@ public abstract class AbstractSignLabel {
 		int blockX = face.getLowerX() + x * boardRot.getXadjustment() + signRot.getXadjustment();
 		int blockY = face.getLowerY() + y;
 		int blockZ = face.getLowerZ() + x * boardRot.getZadjustment() + signRot.getZadjustment();
-		
+
 		return new PersistableLocation(panelBlocks.getWorld(), blockX, blockY, blockZ);
 	}
 
