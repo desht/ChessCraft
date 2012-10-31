cd09e2c059a630fe8924f20423528c79ed5af3dd
Des Herriott <des.herriott@gmail.com>
2012-10-31 10:22:26 +0000

    Create Game button can now select White or Black

--- src/main/java/me/desht/chesscraft/controlpanel/CreateGameButton.java
+++ src/main/java/me/desht/chesscraft/controlpanel/CreateGameButton.java
@@ -1,24 +1,43 @@
 package me.desht.chesscraft.controlpanel;
 
 import me.desht.chesscraft.chess.ChessGame;
+import me.desht.chesscraft.util.ChessUtils;
 
+import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 
 import chesspresso.Chess;
 
 public class CreateGameButton extends AbstractSignButton {
 
+	private int colour = Chess.WHITE;
+	
 	public CreateGameButton(ControlPanel panel) {
 		super(panel, "createGameBtn", "create.game", 1, 2);
 	}
 	
 	@Override
 	public void execute(PlayerInteractEvent event) {
-		ChessGame.createGame(event.getPlayer(), null, getView(), Chess.WHITE);
+		if (event.getAction() == Action.LEFT_CLICK_BLOCK) {
+			ChessGame.createGame(event.getPlayer(), null, getView(), colour);
+		} else if (event.getAction() == Action.RIGHT_CLICK_BLOCK) {
+			// cycle between "White" and "Black"
+			colour = ChessUtils.otherColour(colour);
+			repaint();
+		}
 	}
 
 	@Override
 	public boolean isEnabled() {
 		return getGame() == null && !getView().isDesigning(); 
+	}
+	
+	@Override
+	protected String[] getCustomSignText() {
+		String[] res = getSignText();
+		
+		res[3] = getIndicatorColour() + ChessUtils.getColour(colour);
+
+		return res;
 	}
 }
